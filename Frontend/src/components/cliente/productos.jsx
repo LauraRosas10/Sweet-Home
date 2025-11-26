94034b3726c3defbd6538d6c0b621b4fb9a862af
LauraRosas10 <lalavanesarosas@gmail.com>
2025-11-26 15:26:58 -0500

    actualizacion

--- Frontend/src/components/cliente/productos.jsx
+++ Frontend/src/components/cliente/productos.jsx
@@ -121,15 +121,8 @@ export default function ProductList({ products, onEdit, onDelete, onToggleStatus
 
     return (
       <div
-        onClick={() => {
-                                                if (product.Stock === 0) return; // ❌ Bloquea el click
-                                                onToggleStatus && onToggleStatus(product.id);
-                                            }}
-
-                                            className={`flex items-center gap-2 px-3 py-1 rounded-full transition-all duration-300
-                                                ${product.Stock === 0 ? "opacity-50 cursor-not-allowed" : "cursor-pointer"}
-                                                ${isAvailable ? "bg-green-100 dark:bg-green-900/50" : "bg-red-100 dark:bg-red-900/50"}
-                                            `}
+        onClick={() => onToggleStatus(product._id)}
+        className={`flex items-center gap-2 px-3 py-1 rounded-full cursor-pointer transition-all duration-300 ${bgColor}`}
       >
         {isAvailable ? (
           <>
