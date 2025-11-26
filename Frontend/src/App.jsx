4f9b62ad63c1742c82391930fb90e9b4b5114536
LauraRosas10 <lalavanesarosas@gmail.com>
2025-11-26 16:21:17 -0500

    recargar 1

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,17 +1,19 @@
 import React from "react";
 import { BrowserRouter  } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
+
 import AppRoutes from "./routes/routes.jsx";
 import { ThemeProvider } from "./components/thema.jsx";
 import { CartProvider } from './context/CartContext.jsx';
 const App = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <CartProvider>
       <ThemeProvider>
         <AppRoutes/>
       </ThemeProvider>
       </CartProvider>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 
