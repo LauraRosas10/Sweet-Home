682b47286be9b1f4bfcba8be354dba7874d82630
LauraRosas10 <lalavanesarosas@gmail.com>
2025-11-27 10:50:40 -0500

    actualizacion rend

--- Backend/app.js
+++ Backend/app.js
@@ -1,12 +1,13 @@
-import'dotenv/config';
+import 'dotenv/config';
 import express from 'express';  
 import cors from 'cors';
-import bodyParser from 'body-parser';
 import session from "express-session";
 import passport from "./config/googleAuth.js";
 import dbClient from './config/dbClient.js';
+import fetch from "node-fetch";
 
-// Importar rutas
+
+// Rutas
 import routersUsuario from './routers/usuariosR.js';
 import routersCategoria from './routers/categoriasR.js';
 import routerspedidos from './routers/pedidosR.js';
@@ -14,20 +15,18 @@ import routerProducto from './routers/productosR.js';
 import googleAuthRoutes from "./routers/googleR.js";
 
 const app = express();
-app.use(express.json({ limit: '50mb' })); 
 
-// Aumenta el límite para datos codificados de URL (si los usas)
+// Body parsers correctos
+app.use(express.json({ limit: '50mb' })); 
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
-// Middlewares básicos
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+// CORS
 app.use(cors({
     origin: process.env.FRONTEND_URL || 'http://localhost:5173',
     credentials: true
 }));
 
-// Configuración de sesión y autenticación
+// Sesión y passport
 app.use(session({
     secret: process.env.JWT_TOKEN_SECRET,
     resave: false,
@@ -48,7 +47,7 @@ app.use('/api/categorias', routersCategoria);
 app.use('/api/pedidos', routerspedidos);
 app.use("/auth", googleAuthRoutes);
 
-// Manejo de errores global
+// Manejo errores
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).json({ 
@@ -58,14 +57,15 @@ app.use((err, req, res, next) => {
 });
 
 // Iniciar servidor
-try {
-    const PORT = process.env.PORT || 5100;
-    app.listen(PORT, ()=> console.log('Servidor activo en el puerto ' + PORT))
-} catch (e) {
-    console.log(e)
-}
+const PORT = process.env.PORT || 5100;
+app.listen(PORT, () => console.log('Servidor activo en el puerto ' + PORT));
 
-process.on('SIGINT', async() =>{
+// Cerrar DB
+process.on('SIGINT', async () =>{
     dbClient.cerrarConexion();
     process.exit(0);
 });
+
+setInterval(() => {
+  fetch("https://sweet-home-46ww.onrender.com/api/productos");
+}, 8 * 60 * 1000);
